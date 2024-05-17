369419bedb8663c41634267cd55aacadb854c8b6
Jhonatan Silva <jhonatanfdvk@gmail.com>
2024-05-17 19:31:53 -0300

    Alguns exercicios resolvidos

---
+++ SQLQuery1.sql
@@ -0,0 +1,33 @@
+SELECT Nome, Ano FROM Filmes
+
+SELECT Nome, Ano, Duracao FROM Filmes 
+ORDER BY Ano ASC
+
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Nome = 'DE Volta para o Futuro'
+
+SELECT * FROM Filmes
+WHERE Ano = 1997
+
+SELECT * FROM Filmes 
+WHERE Ano > 2000
+
+SELECT * FROM Filmes 
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC
+
+SELECT * FROM Atores
+WHERE Genero = 'M'
+
+SELECT * FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+
+
+
+
+
+
+
+
